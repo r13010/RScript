e07c8305cb92db5b71592196843f906e9652bae2
Valentin Alexăndroae <the.r001010@gmail.com>
2022-08-20 19:08:11 +0300

    update
    update

--- Resources/InstallArch/Full/2-full-setup-mirrors.sh
+++ Resources/InstallArch/Full/2-full-setup-mirrors.sh
@@ -14,9 +14,33 @@ echo -ne "
 
 pacman -S --noconfirm --needed reflector
 cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.bak
-echo "RSCRIPT: Starting reflector.service"
-systemctl start reflector.service
-journalctl -u reflector.service
+
+options=("Romania" "USA" "Another")
+select opt in "${options[@]}"
+do
+    case $opt in
+        "Romania")
+            reflector --country romania --latest 200 --sort rate --save /etc/pacman.d/mirrorlist
+            break
+        "USA")
+            reflector --country usa --latest 200 --sort rate --save /etc/pacman.d/mirrorlist
+            break
+            ;;
+        "Another")
+            reflector --latest 200 --sort rate --save /etc/pacman.d/mirrorlist
+            break
+            ;;
+echo -ne "
+--------------------------------------------------------------------------------
+    RSCRIPT - Quitting...
+--------------------------------------------------------------------------------
+"
+            break
+            ;;
+        *) echo "Invalid option";;
+    esac
+done
+reflector --country romania --latest 200 --sort rate --save /etc/pacman.d/mirrorlist
 
 }
 
