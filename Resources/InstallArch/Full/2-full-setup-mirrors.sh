ef3abd7beaa538ac8c6de42bd465097b834d0fa1
Valentin Alexăndroae <the.r001010@gmail.com>
2022-08-20 19:21:37 +0300

    up

--- Resources/InstallArch/Full/2-full-setup-mirrors.sh
+++ Resources/InstallArch/Full/2-full-setup-mirrors.sh
@@ -4,36 +4,6 @@
 bash Resources/Others/rsplash.sh
 
 # Setup mirrors
-select_country() {
-
-options=("Romania" "USA" "Another")
-select opt in "${options[@]}"
-do
-case $opt in
-    "Romania")
-        reflector --country romania --latest 200 --sort rate --save /etc/pacman.d/mirrorlist
-        break
-        ;;
-    "USA")
-        reflector --country usa --latest 200 --sort rate --save /etc/pacman.d/mirrorlist
-        break
-        ;;
-    "Another")
-        break
-        ;;
-echo -ne "
---------------------------------------------------------------------------------
-    RSCRIPT - Quitting...
---------------------------------------------------------------------------------
-"
-        break
-        ;;
-    *) echo "Invalid option";;
-    esac
-done
-
-}
-
 mirrors() {
 
 echo -ne "
@@ -50,7 +20,26 @@ echo -ne "
     RSCRIPT - What country are you in?
 --------------------------------------------------------------------------------
 "
-select_country
+
+options=("Romania" "USA" "Another")
+select opt in "${options[@]}"
+do
+case $opt in
+    "Romania")
+        reflector --country romania --latest 200 --sort rate --save /etc/pacman.d/mirrorlist
+        break
+        ;;
+    "USA")
+        reflector --country usa --latest 200 --sort rate --save /etc/pacman.d/mirrorlist
+        break
+        ;;
+    "Another")
+        break
+        ;;
+    *) echo "Invalid option";;
+    esac
+done
+
 }
 
 ### Define script logic
