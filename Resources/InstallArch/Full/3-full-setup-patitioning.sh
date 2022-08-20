4a403b6713f40dc8ec9fd0f80b9ba4750f81b36a
Valentin Alexăndroae <the.r001010@gmail.com>
2022-08-20 21:59:57 +0300

    up

--- Resources/InstallArch/Full/3-full-setup-patitioning.sh
+++ Resources/InstallArch/Full/3-full-setup-patitioning.sh
@@ -4,20 +4,6 @@
 bash Resources/Others/rsplash.sh
 
 # Setup partitioning
-
-choose_partition() {
-
-echo -ne "
---------------------------------------------------------------------------------
-    RSCRIPT - Write the drive you want to install to:
---------------------------------------------------------------------------------
-"
-read -rs -p "My desired partition is /dev/" DRIVE
-echo "RSCRIPT: The selected drive is $DRIVE"
-
-
-}
-
 partitioning() {
 
 echo -ne "
@@ -31,23 +17,34 @@ fdisk -l
 
 echo -ne "
 --------------------------------------------------------------------------------
-    RSCRIPT: Arch needs to be installed to a drive, you now can:
+    RSCRIPT: The current drive selected is /dev/$DRIVE. Is this correct?
 --------------------------------------------------------------------------------
 "
-options=("Choose a drive" "Wait, show me all my drives again")
+options=("Yes, DELETE ALL DATA from this drive and install Arch" "Choose another drive" "Wait, show me all my drives again")
 select opt in "${options[@]}"
 do
     case $opt in
-        "Choose a drive")
+        "Yes, DELETE ALL DATA from this drive and install Arch")
             break
             ;;
+        "Choose another drive")
+            read -rs -p "My desired partition is /dev/" DRIVE
+echo -ne "
+--------------------------------------------------------------------------------
+    RSCRIPT: The current drive selected is /dev/$DRIVE. Is this correct?
+--------------------------------------------------------------------------------
+"
+            ;;
         "Wait, show me all my drives again")
+            clear
             fdisk -l
             sleep 1s
             ;;
         *) echo "Invalid option";;
     esac
 done
+
+echo "RSCRIPT: The selected drive is $DRIVE"
 
 }
 
