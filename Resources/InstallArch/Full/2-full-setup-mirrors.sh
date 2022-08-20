641838952af28334f153d4dcd79a437d29066139
Valentin Alexăndroae <the.r001010@gmail.com>
2022-08-20 19:54:14 +0300

    up

--- Resources/InstallArch/Full/2-full-setup-mirrors.sh
+++ Resources/InstallArch/Full/2-full-setup-mirrors.sh
@@ -18,7 +18,7 @@ cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.bak
 iso=$(curl -4 ifconfig.co/country-iso)
 echo "RSCRIPT: Country detected: $iso"
 echo "RSCRIPT: Starting reflector..."
-reflector --country $iso -f 5 --latest 200 --sort rate --save /etc/pacman.d/mirrorlist
+reflector --country $iso --latest 200 --sort rate --save /etc/pacman.d/mirrorlist
 
 }
 
