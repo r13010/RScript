5291a68b286d55f2851cd073b2a667d256011979
Valentin Alexăndroae <the.r001010@gmail.com>
2022-08-20 23:58:58 +0300

    up

--- Resources/InstallArch/Full/2-full-setup-mirrors.sh
+++ Resources/InstallArch/Full/2-full-setup-mirrors.sh
@@ -11,7 +11,7 @@ echo -ne "
     RSCRIPT - 2-full-setup-mirrors
 --------------------------------------------------------------------------------
 "
-
+# Refreshing mirrors
 pacman -S --noconfirm --needed reflector curl
 cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.bak
 
@@ -22,6 +22,14 @@ reflector --country $iso --latest 200 --sort rate --save /etc/pacman.d/mirrorlis
 
 sed -i 's/^#ParallelDownloads/ParallelDownloads/' /etc/pacman.conf
 
+# Changing compression settings and makeflags
+nc=$(grep -c ^processor /proc/cpuinfo)
+TOTAL_MEM=$(cat /proc/meminfo | grep -i 'memtotal' | grep -o '[[:digit:]]*')
+
+if [[  $TOTAL_MEM -gt 8000000 ]]; then
+sed -i "s/#MAKEFLAGS=\"-j2\"/MAKEFLAGS=\"-j$nc\"/g" /etc/makepkg.conf
+sed -i "s/COMPRESSXZ=(xz -c -z -)/COMPRESSXZ=(xz -c -T $nc -z -)/g" /etc/makepkg.conf
+fi
 }
 
 ### Define script logic
--- Resources/InstallArch/Full/3-full-setup-patitioning.sh
+++ Resources/InstallArch/Full/3-full-setup-patitioning.sh
@@ -12,9 +12,10 @@ echo -ne "
 --------------------------------------------------------------------------------
 "
 
-pacman -S --noconfirm --needed fdisk
+pacman -S --noconfirm --needed fdisk parted
 fdisk -l
-
+parted /dev/sda print devices
+#
 echo -ne "
 --------------------------------------------------------------------------------
     RSCRIPT: The current drive selected is /dev/$DRIVE. Is this correct?
@@ -38,16 +39,54 @@ echo -ne "
         "Wait, show me all my drives again")
             clear
             fdisk -l
+            parted /dev/sda print devices
             sleep 1s
             ;;
         *) echo "Invalid option";;
     esac
 done
 
-echo "RSCRIPT: The selected drive is $DRIVE"
+echo "RSCRIPT: The selected drive is /dev/$DRIVE"
+sleep 1s
+umount -A --recursive /mnt # Make sure everything is unmounted before we start
+
+# Partition the drive as for UEFI or BIOS
+if [[ ! -d "/sys/firmware/efi" ]]; then
+    echo pula bios
+else
+    # UEFI SYSTEM
+    parted /dev/$DRIVE print -s # Print it first
+    echo "RSCRIPT: Making partition table gpt..."
+    parted /dev/$DRIVE mklabel gpt -s # Make it gpt
+    echo "RSCRIPT: Making efi partition..."
+    parted /dev/$DRIVE mkpart "EFI" fat32 1mb 301mb -s # Make efi
+    parted /dev/$DRIVE set 1 boot on -s
+    echo "RSCRIPT: Making swap partition..."
+    parted /dev/$DRIVE mkpart "SWAP" linux-swap 301mb 4397mb -s # Make swap
+    echo "RSCRIPT: Making root partition..."
+    parted /dev/$DRIVE mkpart "ROOT" ext4 4397mb 100% -s # Make root
+
+    DRIVE1=$DRIVE"1"
+    DRIVE2=$DRIVE"2"
+    DRIVE3=$DRIVE"3"
+
+    echo "RSCRIPT: Formatting efi patition..."
+    mkfs.fat -F 32 /dev/$DRIVE1 # Format efi
+    echo "RSCRIPT: Formatting swap patition..."
+    mkswap /dev/$DRIVE2 # Format swap
+    echo "RSCRIPT: Formatting root patition..."
+    mkfs.ext4 /dev/$DRIVE3 # Format root as ext4
+
+    echo "RSCRIPT: Mounting partitions..."
+    mount /dev/$DRVIE3 /mnt
+    mount --mkdir /dev/$DRIVE1 /mnt/boot
+    swapon /dev/$DRIVE2
+
+    parted /dev/$DRIVE print -s # Print it again
+fi
+
 
 }
 
 ### Define script logic
 partitioning
-choose_partition
